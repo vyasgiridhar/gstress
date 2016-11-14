a82773ea6489d02c5db3f2d87034a4cf2e35c62b
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-11-14 09:50:38 +0530

    Added CLI manager

--- main.go
+++ main.go
@@ -1,36 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"github.com/urfave/cli"
+	"github.com/vyasgiridhar/gstress/src"
 	"os"
 	"runtime"
-	"github.com/urfave/cli"
 )
 
 func main() {
-	var cpu int
+	var (
+		cpu     int
+		io      int
+		timeout int
+		hdd     int
+	)
 
 	app := cli.NewApp()
 
-	app.Flags = []cli.Flag
+	app.Flags = []cli.Flag{
 		cli.IntFlag{
 			Name:        "cpu",
-			Value:       runtime.GOMAXPROCS,
+			Value:       runtime.GOMAXPROCS(0),
 			Usage:       "Number of cpus to use for stress test",
-			Destination: cpu&,
+			Destination: &cpu,
+		},
+		cli.IntFlag{
+			Name:        "io",
+			Value:       4,
+			Usage:       "Spawn N goroutines working on Sync()",
+			Destination: &io,
+		},
+		cli.IntFlag{
+			Name:        "hdd",
+			Value:       4,
+			Usage:       "Spawn N goroutines on Write()",
+			Destination: &hdd,
+		},
+		cli.IntFlag{
+			Name:        "timeout",
+			Value:       0,
+			Usage:       "timeout after N seconds",
+			Destination: &timeout,
 		},
 	}
 
 	app.Action = func(c *cli.Context) error {
-		name := "someone"
-		if c.NArg() > 0 {
-			name = c.Args()[0]
-		}
-		if language == "spanish" {
-			fmt.Println("Hola", name)
-		} else {
-			fmt.Println("Hello", name)
-		}
+
+		cpu := c.Int("cpu")
+		io := c.Int("io")
+		hdd := c.Int("hdd")
+		timeout := c.Int("timeout")
+
+		src.Spawner(cpu, io, hdd, timeout)
+
 		return nil
 	}
 
